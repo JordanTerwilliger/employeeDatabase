8dd336ce1c26943d413dcf8a9b6681e1b0b4261d
JordanTerwilliger <jordanterwilliger@hotmail.com>
2025-02-07 15:30:30 -0500

    4th commit

--- main.cpp
+++ main.cpp
@@ -1,89 +1,62 @@
 #include <iostream>
 #include <string.h>
+#include <string>
 #include <stdlib.h>
 #include <vector>       
 using namespace std;
 
 class Employee{
+    public:
+    Employee(string name, string title, double wage){     //Constuctor
+        this->name = name;
+        this->title = title;
+        this->wage = wage;
+    }
+    string getName(){return name;}
+    string getTitle(){return title;}
+    double getWage(){return wage;}
+
     private:
     string name;
     string title;
     double wage;
-    static int employeeCount;
-
+};
+class EmployeeApps{
     public:
-    //Constuctor
-    Employee(string constructorName, string constructorTitle, double constructorWage);
+    vector<Employee> employees;
+    void createEmployees(){
+        employees.push_back(Employee("Bob","Manager", 40));
+        employees.push_back(Employee("John","Line Cook", 20));
+        employees.push_back(Employee("Mark","Cashier", 8));
+        employees.push_back(Employee("Matthew","Cashier", 10));
+        employees.push_back(Employee("Sarah", "Cashier", 7.25));
+    }
+    void calculateTotalWages(){
+        int employeeHours;
+        vector<double> hours; //Creates a vector to hold employee's hours
+        for (size_t i = 1; i <= employees.size(); i++){
+            cout << "Enter the amount of hours worked for employee #" << i + 1 << " (" << employees[i].getName() << "):\n";
+            cin >> employeeHours;
+            hours.push_back(employeeHours);
+        }
+        double totalWages = 0;
+        for (size_t i = 0; i < employees.size(); i++){
+            totalWages += hours[i] * employees[i].getWage();
+        }
 
-    void setEmployeeCount(int employeeCount);
-    int getEmployeeCount() const;
-    void setName(string name);
-    string getName() const;
-    void setTitle(string name);
-    string getTitle() const;
-    void setWage(double wage);
-    double getWage() const;
+        cout << "The total wages for all employees is: $" << totalWages << endl;
+        system("pause");
+    }
+    void printEmployees(){
+        for (size_t i = 0; i < employees.size(); i++){
+            cout << "Employee #" << i + 1 << ": " << employees[i].getName() << ", Title: " << employees[i].getTitle() << ", Wage: $" << employees[i].getWage() << "/hr\n";
+        }
+        system("pause");
+    }
 };
 
-int Employee::employeeCount = 5;
-
-//Getters and Setters
-    void Employee::setEmployeeCount(int newEmployeeCount){
-        Employee::employeeCount = newEmployeeCount;
-    }
-    int Employee::getEmployeeCount() const {
-        return Employee::employeeCount;
-    }
-    void Employee::setName(string name){
-        
-    }
-    string Employee::getName() const {
-
-        return Employee::name;
-    }
-    void Employee::setTitle(string name){
-
-    }
-    string Employee::getTitle() const {
-        return Employee::title;
-    }
-    void Employee::setWage(double wage){
-
-    }
-    double Employee::getWage() const {
-        return Employee::wage;
-    }
 
 
-void createEmployee(){
-    Employee e1("Bob","Manager",40);
-    Employee e2("John","Line Cook", 20);
-    Employee e3("Mark","Cashier", 8);
-    Employee e4("Matthew","Cashier", 10);
-    Employee e5("Sarah", "Cashier", 7.25);
-
-}
-void calculateTotalWages(){
-    int employeeHours;
-    int employeeCount = Employee::getEmployeeCount();
-    vector<double> hours = {}; //Creates a vector to hold employee's hours
-    for (size_t i = 1; i <= employeeCount; i++){
-        cout << "Enter the amount of hours worked for employee #" << i << "\n";
-        cin >> employeeHours;
-        hours.push_back(employeeHours);
-    }
-    
-    system("pause");
-}
-
-void printEmployees(){
-    int employeeCount = Employee::getEmployeeCount();
-    for (int i = 0; i < employeeCount; i++){
-        //cout << getTitle;
-    }
-    system("pause");
-    
-}
 void desc(){
     cout << "\nHello, this application is designed to manage employees and their wages.\n"; 
 }
@@ -95,6 +68,7 @@ void options(){
 
 }
 int main(){
+    EmployeeApps apps;
     desc();
     string input;
     while (true)
@@ -102,13 +76,12 @@ int main(){
         options();
         cin >> input;
         int userSelection = atoi(input.c_str());
-        switch (userSelection)
-        {
+        switch (userSelection){
         case 1: //Print employees in list.
-            printEmployees();
+            apps.printEmployees();
             break;
         case 2: // Calc total wages.
-            calculateTotalWages();
+            apps.calculateTotalWages();
             break;
         case 3: //Exit application.
             return 0;
