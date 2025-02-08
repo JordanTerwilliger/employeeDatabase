bd5e88bb645c58b44b45306a8216c82bb5b553db
JordanTerwilliger <jordanterwilliger@hotmail.com>
2025-02-08 16:46:15 -0500

    first working build . add improper user input exception handling

--- main.cpp
+++ main.cpp
@@ -7,53 +7,47 @@ using namespace std;
 
 class Employee{
     public:
-    Employee(string name, string title, double wage){     //Constuctor
-        this->name = name;
-        this->title = title;
-        this->wage = wage;
-    }
     string getName(){return name;}
     string getTitle(){return title;}
-    double getWage(){return wage;}
+    double getWage(){return wage;};
+
+    Employee(string name, string title, double wage){     //Constuctor
+        this -> name = name;
+        this -> title = title;
+        this -> wage = wage;
+    }
 
     private:
     string name;
     string title;
     double wage;
 };
-class EmployeeApps{
-    public:
-    vector<Employee> employees;
-    void createEmployees(){
-        employees.push_back(Employee("Bob","Manager", 40));
-        employees.push_back(Employee("John","Line Cook", 20));
-        employees.push_back(Employee("Mark","Cashier", 8));
-        employees.push_back(Employee("Matthew","Cashier", 10));
-        employees.push_back(Employee("Sarah", "Cashier", 7.25));
-    }
-    void calculateTotalWages(){
-        int employeeHours;
+
+    
+    void calculateTotalWages(int employeeCount, vector<Employee>& employees){
+        double employeeHours;
         vector<double> hours; //Creates a vector to hold employee's hours
-        for (size_t i = 1; i <= employees.size(); i++){
+        for (size_t i = 0; i < employeeCount; i++){
             cout << "Enter the amount of hours worked for employee #" << i + 1 << " (" << employees[i].getName() << "):\n";
             cin >> employeeHours;
             hours.push_back(employeeHours);
         }
         double totalWages = 0;
-        for (size_t i = 0; i < employees.size(); i++){
-            totalWages += hours[i] * employees[i].getWage();
+        for (size_t i = 0; i < employeeCount; i++){
+            totalWages += hours[i] * employees[i].getWage(); //Total wages = hour of the employee * the hours the employee worked ( user inputted)
         }
 
         cout << "The total wages for all employees is: $" << totalWages << endl;
         system("pause");
     }
-    void printEmployees(){
-        for (size_t i = 0; i < employees.size(); i++){
+
+
+    void printEmployees(int employeeCount, vector<Employee>& employees){
+        for (size_t i = 0; i < employeeCount; i++){
             cout << "Employee #" << i + 1 << ": " << employees[i].getName() << ", Title: " << employees[i].getTitle() << ", Wage: $" << employees[i].getWage() << "/hr\n";
         }
         system("pause");
     }
-};
 
 
 
@@ -68,7 +62,13 @@ void options(){
 
 }
 int main(){
-    EmployeeApps apps;
+    vector<Employee> employees = {
+        Employee("Bob","Manager", 40),
+        Employee("John","Line Cook", 20),
+        Employee("Mark","Cashier", 8),
+        Employee("Matthew","Cashier", 10),
+        Employee("Sarah", "Cashier", 7.25),
+    };
     desc();
     string input;
     while (true)
@@ -78,10 +78,10 @@ int main(){
         int userSelection = atoi(input.c_str());
         switch (userSelection){
         case 1: //Print employees in list.
-            apps.printEmployees();
+            printEmployees(employees.size(), employees);
             break;
         case 2: // Calc total wages.
-            apps.calculateTotalWages();
+            calculateTotalWages(employees.size(),employees);
             break;
         case 3: //Exit application.
             return 0;
